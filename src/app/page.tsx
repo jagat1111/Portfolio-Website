535a45f8ee489f5e1e85d6c1843511c2c62c41b1
Jatin Yadav <yadavjatin923@gmail.com>
2025-04-24 06:16:45 +0530

    Update configuration for GitHub Pages deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import About from "@/common/components/sections/about";
-import Contact from "@/common/components/sections/contact";
-import Experience from "@/common/components/sections/experience";
-import Hero from "@/common/components/sections/hero";
-import Projects from "@/common/components/sections/projects";
-import Skills from "@/common/components/sections/skills";
+import About from "@/common/components/sections/about/index";
+import Contact from "@/common/components/sections/contact/index";
+import Experience from "@/common/components/sections/experience/index";
+import Hero from "@/common/components/sections/hero/index";
+import Projects from "@/common/components/sections/projects/index";
+import Skills from "@/common/components/sections/skills/index";
 import Certificate from "@/common/components/sections/Certificate/Certificate";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center p-0">
